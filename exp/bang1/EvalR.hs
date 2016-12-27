5ff76ce775e90816d6f5246bdc04def1d32d772d
Yun-Yan Chi <jaiyalas@gmail.com>
2016-12-27 21:21:14 +0800

    .

--- exp/bang1/EvalR.hs
+++ exp/bang1/EvalR.hs
@@ -25,31 +25,16 @@ vmTrans (Lit v) = Lit v
 vmTrans (Atom (Var va)) = mat va
 vmTrans (Prod vt1 vt2) = Prod (mvTrans vt1) (mvTrans vt2)
 vmTrans (NatS vt) = NatS (mvTrans vt)
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 
-trans :: (Expr, Expr) -> Expr
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-trans (expr, Term vt) = undefined
-trans (expr, Return vt) = undefined
-trans (expr, Begin (Match vt (c:cs))) = undefined
-trans (expr, Begin (MatEq vt c1 c2)) = undefined
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-trans (expr, LetIn mt (Left vt) e) =
-    let mt' = vmTrans vt
-        vt' = mvTrans mt
-trans (expr, LetIn mt (Right fapp) e) = undefined
-trans (expr, Match vt (c : cs)) = undefined
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-trans (expr, DupIn mt vt e) = undefined
-trans (expr, MatEq mt case1 case2) = undefined
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-trans (expr, Match vt []) = undefined
+
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- evalR :: Env -> Expr -> (Val, Env)
+evalR :: Env -> Expr -> (Val, Env)
 -- evalR :: Env -> Expr -> Env
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- evalR env (Term vt) = undefined
+evalR env (Term vt) = appSigma env vt
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 -- -- whenever Return is reached, there must have exactly one variable in env
 -- -- followed by the setting in Func, this variable should have name as "#0"
