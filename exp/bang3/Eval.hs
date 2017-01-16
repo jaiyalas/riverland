1e5f5bff4b7af2240ff91e5d12ea98ddef20428c
Yun-Yan Chi <jaiyalas@gmail.com>
2017-01-16 20:52:12 +0800

    new branch: bang3

---
+++ exp/bang3/Eval.hs
@@ -0,0 +1,60 @@
+module Eval where
+--
+import Expr
+import Env
+import Func
+import Pat
+--
+eval :: Env -> Expr -> (Val, Env)
+--
+eval env (Term vt)    = reveal Linear env vt
+--
+eval env (LetIn mt (Left vt) e) =
+    let (val, env') = reveal Linear env vt
+        newEnv = update Linear env' mt val
+    in eval newEnv e
+--
+eval env (LetIn mt (Right (funame, vt)) e) =
+    let (Closure fbody, _) = reveal Normal env (var funame)
+        (argVal, env') = reveal Linear env vt
+        localEnv = Env [(Var "#0", argVal)] (getNCtx env)
+        -- localEnv = update Linear env' (mat "#0") argVal
+        (res, _) = eval localEnv fbody
+        newEnv = update Linear env' mt res
+    in eval newEnv e
+eval env (DupIn (Prod (Atom (Mat ma1)) (Atom (Mat ma2))) (Atom va) e) =
+        let (val, env') = raccess Linear env va
+            newEnv = (Var ma2, val) `consL` ((Var ma1, val) `consL` env')
+        in eval newEnv e
+--
+eval env (Match vt cases) =
+    let (val, env1) = reveal Linear env vt
+        (env2, e) = matching val cases
+    in eval (env1 `mappend` env2) e
+--
+eval env (MatEq vt case1 case2) = case reveal Linear env vt of
+    ((Pair val1 val2), env2) ->
+        if val1 == val2
+            then case case1 of
+                (Atom (Mat ma) :~> e1) ->
+                    eval ((Var ma, val2) `consL` env2) e1
+                (NatS (Atom (Mat ma)) :~> e2) ->
+                    eval ((Var ma, redN val1) `consL` env2) e2
+                (pat :~> _) -> error $
+                    "<<eval | Illegal pattern>>\n"++
+                    "\t"++(show pat)++" is illegal within MatEq-(case1)"
+            else case case2 of
+                (Prod mt1 mt2 :~> e1) ->
+                    let newEnv = update Linear (update Linear env2 mt1 val1) mt2 val2
+                    in eval newEnv e1
+                (pat :~> _) -> error $
+                    "<<eval | Illegal pattern>>\n"++
+                    "\t"++(show pat)++" is illegal within MatEq-(case2)"
+    otherwise -> error $
+        "<<eval | Illegal value>>\n"++
+        "\t"++(show vt)++" is illegal within MatEq"
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+eval _ _ = error $
+    "<<eval | Unknown>>\n"++
+    "eval failed"
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- --
---
+++ exp/bang3/Rval.hs
@@ -0,0 +1,92 @@
+module Rval where
+--
+import Expr
+import Env
+import Func
+import Pat
+--
+rval :: (Val, Env) -> Expr -> Env
+--
+rval (v, env) (Term vt) = update Linear env (vmTrans vt) v
+--
+rval (v, env) (LetIn mt (Left vt) e) =
+    let midEnv = rval (v, env) e
+        (val, env') = reveal Linear midEnv (mvTrans mt)
+    in update Linear env' (vmTrans vt) val
+--
+rval (v, env) (LetIn mt (Right (fname, vt)) e) =
+    let midEnv = rval (v, env) e
+        (val, env') = reveal Linear midEnv (mvTrans mt)
+        (Closure fbody, _) = reveal Normal env (var fname)
+        -- clear local env?
+        localEnv = rval (val, mempty) fbody
+        -- ???
+        (vFunIn, _)= raccess Linear localEnv (Var "#0")
+    in update Linear env' (vmTrans vt) vFunIn
+--
+rval (v, env) (Match vt []) = error $
+    "<<rval | Case exhausted>>\n"++
+    "\tNo pattern can be rev-matched"
+rval (v, env) (Match vt (mt :~> cexp : cases)) =
+    if oracle v cexp
+        then let midEnv = rval (v, env) cexp
+                 (patVal, finEnv) = reveal Linear midEnv (mvTrans mt)
+             in update Linear finEnv (vmTrans vt) patVal
+        else rval (v, env) (Match vt cases)
+--
+rval (v, env) (DupIn (Prod mtl mtr) vt e) =
+    let midEnv = rval (v, env) e
+        (lVal, midEnv2) = reveal Linear midEnv (mvTrans mtl)
+        (rVal, finEnv) = reveal Linear midEnv2 (mvTrans mtr)
+    in if lVal == rVal
+        then update Linear finEnv (vmTrans vt) rVal
+        else error $
+            "<<rval | Illegal values>>\n"++
+            "\tReversing DupIn failed with: "++
+            "\t\t("++(show lVal)++"=/="++(show rVal)++")"
+rval (v, env) (MatEq vt case1 case2) =
+    rval (v, env) (Match vt [case1,case2])
+rval _ _ = error $
+    "<<rval | Unknown>>"
+
+
+{-
+The `oracle` is an extension of `rMatch`.
+`oracle` allow to r-matching upon expr-level.
+-}
+oracle :: Val -> Expr -> Bool
+oracle (Pair vl vr) (DupIn mt vt expr)
+    | vl == vr = rMatch (Pair vl vr) (dss expr)
+    | otherwise = False
+oracle v expr = rMatch v (dss expr)
+-- reversed matching
+rMatch :: Val -> VTerm -> Bool
+rMatch (Closure _) _ = False
+rMatch _ (Atom _) = True
+-- 0 <m> 0
+rMatch (N Z) (Lit (N Z)) = True
+-- S n <m> S n
+rMatch (N (S n)) (Lit (N (S m))) = rMatch (N n) (Lit (N m))
+-- 0 <m> S n
+rMatch (N Z) (NatS vt) = False
+-- S n <m> S n
+rMatch (N (S n)) (NatS vt) = rMatch (N n) vt
+rMatch (B b) (Lit (B c)) = if b == c then True else False
+rMatch (Pair v1 v2) (Prod vt1 vt2) =
+    (rMatch v1 vt1) && (rMatch v2 vt2)
+rMatch _ _ = False
+
+-- headConstructor, aka de-structural-syntax
+dss :: Expr -> VTerm
+dss (Term vt) = vt
+dss (LetIn mt (Left vt) expr) = dss expr
+dss (LetIn mt (Right fapp) expr) = dss expr
+dss (DupIn mt vt expr) = dss expr
+dss (Match vt cases) = error $
+    "<<dss | Illegal syntax>>\n"++
+    "\t\"Match\" cannot be DSS-fied"
+dss (MatEq vt case1 case2) = error $
+    "<<dss | Illegal syntax>>\n"++
+    "\t\"MatEq\" cannot be DSS-fied"
+
+-- a case-fliper will be helpful
